c09322d7e5ae5befd9a6c4912be5e99e6932217f
Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
2026-01-07 23:16:52 +0530

    update licenses and remove uthash from src/sym, already in include
    Signed-off-by: Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>

--- sherlock/test/test.c
+++ sherlock/test/test.c
@@ -2,7 +2,7 @@
  * Sherlock - A Minimal Debugger
  * Part of the Sherlock project
  *
- * Copyright (c) 2025-26 Mohammad Shehar Yaar Tausif
+ * Copyright (c) 2025-26 Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
  *
  * This file is licensed under the MIT License.
  */
